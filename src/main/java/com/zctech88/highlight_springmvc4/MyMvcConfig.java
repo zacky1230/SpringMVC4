3441210fb1214fa77041370aecc983ba68908b63
zacky1230 <zachary31@outlook.com>
2018-01-17 18:02:53 +0800

    拦截器配置

--- src/main/java/com/zctech88/highlight_springmvc4/MyMvcConfig.java
+++ src/main/java/com/zctech88/highlight_springmvc4/MyMvcConfig.java
@@ -1,28 +1,45 @@
 package com.zctech88.highlight_springmvc4;
 
+import com.zctech88.highlight_springmvc4.interceptor.DemoInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc//开启一些默认配置
 @ComponentScan("com.zctech88.highlight_springmvc4")
-public class MyMvcConfig {
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
 
     /**
      * JSP 的 ViewResolver,用来映射路径
+     *
      * @return
      */
     @Bean
-    public InternalResourceViewResolver viewResolver(){
+    public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new
                 InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/classes/views/");
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass(JstlView.class);
         return viewResolver;
+    }
+
+    @Bean
+    public DemoInterceptor demoInterceptor() {
+        return new DemoInterceptor();
+    }
+
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(demoInterceptor());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations
+                ("classpath:/assets/");
     }
 }
